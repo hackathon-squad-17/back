ec33a71629d2edb7f5152850bd9adf6295fc3648
SaritaRimes <saritamrimes@gmail.com>
2022-04-06 16:44:42 -0300

    configura MySQL com JPA e Hibernate

--- src/main/java/com/fcamara/hackathonbackend/model/Usuario.java
+++ src/main/java/com/fcamara/hackathonbackend/model/Usuario.java
@@ -1,13 +1,26 @@
 package com.fcamara.hackathonbackend.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String email;
 
     public Usuario() {}
 
+    /* Metodos de acessibilidade */
     public String getNome() {
         return nome;
     }
@@ -30,5 +43,19 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+    /* ------------------------- */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario)) return false;
+        Usuario usuario = (Usuario) o;
+        return id == usuario.id && nome.equals(usuario.nome) && password.equals(usuario.password) && email.equals(usuario.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, password, email);
     }
 }
