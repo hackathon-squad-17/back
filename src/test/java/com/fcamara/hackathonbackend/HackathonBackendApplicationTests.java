4e492f66d666b196246317c6e6c81591b446cf16
SaritaRimes <saritamrimes@gmail.com>
2022-04-06 15:38:52 -0300

    adiciona o projeto recem criado

---
+++ src/test/java/com/fcamara/hackathonbackend/HackathonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamara.hackathonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
